3d5c28a95f0ddfd73308ac7efdaed778d677f9f6
David Binaud <david.binaud@etu.umontpellier.fr>
2022-10-06 20:35:22 +0200

    EDBD - TDs

--- S5/HAI708L/TD2/Td2-2.sql
+++ S5/HAI708L/TD2/Td2-2.sql
@@ -1,7 +1,6 @@
 -- SET AUTOTRACE ON
 
--- Q3
-
+-- Q2
 -- SELECT nom
 -- FROM ville
 -- WHERE insee=1293;
@@ -10,18 +9,52 @@
 -- FROM ville
 -- WHERE insee='1293';
 
--- Q4
+
+-- Q3
 -- ALTER TABLE ville add primary key(insee);
 
+
+-- Q4
+-- SELECT nom
+-- FROM ville
+-- WHERE insee=1293;
+
+-- SELECT nom
+-- FROM ville
+-- WHERE insee='1293';
+
+
+-- Q5
 -- SELECT d.nom
 -- FROM ville v
 -- JOIN departement d ON v.dep = d.id 
 -- WHERE insee='1293';
 
+
+-- Q6
 -- SELECT d.nom
 -- FROM ville v
 -- JOIN departement d ON v.dep = d.id;
 
-SELECT /*+ use_nl(v d)*/ d.nom 
+
+-- Q7
+-- SELECT /*+ use_nl(v d)*/ d.nom 
+-- FROM ville v
+-- JOIN departement d ON v.dep = d.id;
+
+
+-- Q8
+-- create index idx_dep_ville on ville (dep);
+SELECT d.nom
 FROM ville v
-JOIN departement d ON v.dep = d.id;
+JOIN departement d ON v.dep = d.id 
+WHERE insee='1293';
+
+-- SELECT d.nom
+-- FROM ville v
+-- JOIN departement d ON v.dep = d.id;
+
+-- Q9
+-- SELECT v.nom, d.nom, r.nom
+-- FROM ville v, departement d, region r
+-- WHERE v.dep = d.id AND d.reg = r.id;
